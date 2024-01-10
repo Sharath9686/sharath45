310ee42d49fbf122455c704d338ce0dfd6996334
Sharath9686 <128211843+Sharath9686@users.noreply.github.com>
2024-01-10 18:58:01 +0530

    Add files via upload

---
+++ AboutUsPage.js
@@ -0,0 +1,81 @@
+
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../ComponentsCSS/AboutUsPage.css';
+import Clinec from '../immagess/dranil.jpg'
+import Primary from '../immagess/drbelehalli.jpg'
+import Emergency from '../immagess/drlatha.jpg'
+import Blood from '../immagess/drdinesh.jpg'
+// import Appointment from './applogo.png'
+
+const AboutourDoctors = [
+  {
+    id: 1,
+    Name: 'Dr. Anil Kumar R',
+    Immage: <img src={Clinec} alt='Kumar'id='kumar' />,
+    AboutDoctor: 'MBBS, MD, FRCP (Edinburgh), FICP,FRSSDI, WHO Fellowship in Diabetology',
+    link: "https://kierbengaluru.karnataka.gov.in/uploads/media_to_upload1674449693.pdf",
+  },
+  {
+    id: 2,
+    Name: 'Dr. Belehalli Pavan',
+    Immage: <img src={Primary} alt='Pavan' id='pavan'  />,
+    AboutDoctor: 'MBBS, DNB-Orthopaedics,Mch - Orthopaedics, MNAMS',
+    link: "https://kierbengaluru.karnataka.gov.in/uploads/media_to_upload1673069723.pdf",
+  },
+  {
+    id: 3,
+    Name: 'Dr. Lalitha R',
+    Immage: <img src={Emergency} alt='Latha' id='latha' />,
+    AboutDoctor: 'MBBS, MD (Internal Medicine), SCE (RCP UK),PGD (Cardiff University, UK),PGD (Diabetes and Clinical Endo) RCP, UK',
+    link: "https://kierbengaluru.karnataka.gov.in/uploads/media_to_upload1672304307.pdf",
+  },
+  {
+    id: 4,
+    Name: '	Dr. Dinesh R.B',
+    Immage: <img src={Blood} alt='Dinesh' id='dinesh' />,
+    AboutDoctor: 'MBBS, DOMS, DNB, MNAMS, FPEC',
+    link: "https://kierbengaluru.karnataka.gov.in/uploads/media_to_upload1675054113.pdf",
+  },
+]
+// const Aboutimformation=[{
+
+//   link:'',
+// }]
+function AboutUsPage() {
+  return (
+    <>
+
+      <section className="about-section">
+        <h2>About our Doctors</h2>
+        <div className='About-Doctors'>
+          {AboutourDoctors.map(service => (
+            <div key={service.id} className="about-card">
+              <div className="about-icon">{service.Immage}</div>
+              <h3>{service.Name}</h3>
+              <p>{service.AboutDoctor}</p>
+              <Link to={service.link} className="about-link">Learn More</Link>
+            </div>
+          ))}
+        </div>
+      </section>
+<div className='about-page'>
+      <div className='about-Team'></div>
+      <hr color="blue" width="100%"></hr>
+      <p className='about-p'>About :-</p>
+      <Link to="/Main/Aboutmanagement"><h4 className='about-Team'> Our Management</h4></Link>
+      <div className='About-Department'>
+      <Link to="/Main/AboutDeportment"><h4 className='About-Department'>Our Department</h4></Link>
+      </div>
+      <div className='About-Clinic'></div>
+      <Link to="/Main/AboutClinic"><h4 className='About-Clinic'>About our Clinic</h4></Link>
+
+      <div className='Feedback'>
+
+      </div>
+      </div>
+    </>
+  )
+}
+export default AboutUsPage;
+
